97bb0c567740dab7d35737f389ec387b86382e5b
traustr146 <abhijeet.iitp@outlook.com>
2018-03-08 21:59:13 +0530

    pug updates

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
+//node modules included
 var express=require('express')
 var stylus=require('stylus')
+var morgan=require('morgan')
+var bodyParser=require('body-parser')
+
+//express instance creation
 var app=express()
+
+//port specification
+var port=3000
 
 //evironment initialization
 var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development'
@@ -13,20 +21,33 @@ app.set('view engine','pug')
 function compile(str,path){
     return stylus(str).set('filename',path);
 }
-
+//stylus middleware
 app.use(stylus.middleware(
     {
     src:__dirname+'/public',
     compile:compile
     }
 ))
+
+//morgan middleware 
+app.use(morgan('dev'))
+
+//badyparser middleware
+app.use(bodyParser.urlencoded({
+    extended:true
+}))
+app.use(bodyParser.json())
+
+
+//static routing for pubic folder
 app.use(express.static(__dirname+'/public'))
 
+//basic routing
 app.get('*',function(req,res){
     res.render('index')
 })
 
-var port =3000
+//port attention
 app.listen(port,function(err){
     if (err){
         console.log(err);
